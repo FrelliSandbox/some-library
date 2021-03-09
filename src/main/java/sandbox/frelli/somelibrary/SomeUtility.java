d8cbb9eb1db28b38c41ffc991093dbbbec5e1e54
Francesco Borg Bonaci <frellibb@gmail.com>
2021-03-09 20:04:15 +0000

    Initialize repository

---
+++ src/main/java/sandbox/frelli/somelibrary/SomeUtility.java
@@ -0,0 +1,34 @@
+package sandbox.frelli.somelibrary;
+
+import java.util.Random;
+
+public class SomeUtility {
+
+    private static final Random r = new Random();
+    private static final String[] responses = {
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes – definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful.",
+    };
+
+    public String magicEightBall() {
+        return responses[r.nextInt(responses.length)];
+    }
+}
